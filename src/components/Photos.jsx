de7b84739d1094292bb15dc65e72830afb0a8794
Shane <sb_94@yahoo.com>
2024-02-22 13:23:15 -0800

    worked on Photos page and styling

---
+++ src/components/Photos.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import HamburgerMenu from './HamburgerMenu';
+import '../App.css';
+
+
+import icecream from '../images/cup-icecream.jpeg';
+import drinkpouch from '../images/drinkpouch.webp';
+import macaron from '../images/macaron.webp';
+import mochi from '../images/mochi-ice-cream-034.jpeg';
+
+
+const Photos = () => {
+  const [selectedPhoto, setSelectedPhoto] = useState(null);
+
+  const photos = [
+    { id: 1, path: icecream, description: 'Single Ice Cream Cups' },
+    { id: 2, path: drinkpouch, description: 'Boba Drink Pouches' },
+    { id: 3, path: macaron, description: 'Macaron Ice Cream Sandwiches' },
+    { id: 4, path: mochi, description: 'Mochi Ice Cream Sandwiches' },
+  ];
+
+  const openModal = (photo) => {
+    setSelectedPhoto(photo);
+  };
+
+  const closeModal = () => {
+    setSelectedPhoto(null);
+  };
+
+  return (
+    <div>
+      <div className="photos-page">
+        <h1 className="photos-heading">PHOTO GALLERY</h1>
+      </div>
+
+      <HamburgerMenu />
+
+      <div className="photos-container">
+        {photos.map((photo) => (
+          <div key={photo.id} className="photo-thumbnail" onClick={() => openModal(photo)}>
+            <img src={photo.path} alt={`Photo ${photo.id}`} />
+            <p>{photo.description}</p>
+            <button className="view-button">View</button>
+          </div>
+        ))}
+      </div>
+
+      {selectedPhoto && (
+        <div className="photo-modal">
+          <img src={selectedPhoto.path} alt={`Photo ${selectedPhoto.id}`} />
+          <p>{selectedPhoto.description}</p>
+          <button onClick={closeModal}>Close</button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Photos;
