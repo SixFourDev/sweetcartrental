6ea8cdf99e4ec1a929082e6e76f427a67b22deca
Shane <sb_94@yahoo.com>
2024-02-22 15:19:33 -0800

    added social links

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -4,16 +4,16 @@ import '../App.css';
 const Socials = () => {
   return (
     <div className="social-icons-container">
-      <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+      <a href="https://www.instagram.com/sweetcartrental/" target="_blank" rel="noopener noreferrer">
         <i className="fab fa-instagram"></i>
       </a>
       <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
         <i className="fab fa-facebook"></i>
       </a>
-      <a href="mailto:your@email.com">
+      <a href="mailto:sweetcartrental.sd@gmail.com">
         <i className="fas fa-envelope"></i>
       </a>
-      <a href="https://buymeacoffee.com" target="_blank" rel="noopener noreferrer">
+      <a href="https://www.buymeacoffee.com/jsicajane" target="_blank" rel="noopener noreferrer">
         <i className="fas fa-coffee"></i>
       </a>
     </div>
